f5dac480e72a3532137577611e6f67a2cdbd21c0
Jarek <dojarka0@gmail.com>
2022-03-13 21:05:37 +0100

    build app

--- src/date.js
+++ src/date.js
@@ -9,16 +9,16 @@ export const date = {
       first: "Jarek",
       second: "Front end developer",
     },
-    imgUrl: "/developer_activity_re_39tg.svg",
+    imgUrl: "developer_activity_re_39tg.svg",
   },
   aboutMe: {
-    imgUrl: "/public/hero.jpg",
+    imgUrl: "public/hero.jpg",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum sit amet metus id tristique. Fusce tristique nulla in elementum egestas. Nam ultrices risus eget eros aliquam, quis mollis enim accumsan. Etiam lobortis efficitur leo, id consequat velit feugiat et. Proin pellentesque tortor quis sem viverra venenatis. Proin vehicula fringilla orci. Cras ultrices augue fringilla dui fermentum blandit. Sed rhoncus nulla sed nibh blandit rhoncus. Morbi sodales ut nisl a consequat.Vestibulum quis quam ex. Morbi euismod, justo eu sagittis mattis, ex massa rhoncus lacus, vel pretium erat neque vitae diam. Nam sed libero id lorem lobortis hendrerit sed vel leo. Maecenas eu massa elit. In ut dignissim odio, ut malesuada dui. Nullam fringilla commodo elit sit amet iaculis. Suspendisse porta purus et rutrum malesuada.",
   },
   myProjects: [
     {
-      imgUrl: "/desktop-design.jpg",
+      imgUrl: "desktop-design.jpg",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisquerutrum sit amet metus id tristique. Fusce tristique nulla inelementum egestas. Aenean commodo turpis sapien, congue egestaslorem commodo sit amet. Quisque accumsan ante sed lacus accumsan dignissim.",
       liveUrl:
@@ -27,7 +27,7 @@ export const date = {
         "https://github.com/ProgramistaJarek/ecommerce-product-page-main",
     },
     {
-      imgUrl: "/desktop-design2.jpg",
+      imgUrl: "desktop-design2.jpg",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisquerutrum sit amet metus id tristique. Fusce tristique nulla inelementum egestas. Aenean commodo turpis sapien, congue egestaslorem commodo sit amet. Quisque accumsan ante sed lacus accumsan dignissim.",
       liveUrl: "https://programistajarek.github.io/advice-generator-app-main/",
@@ -35,14 +35,14 @@ export const date = {
         "https://github.com/ProgramistaJarek/advice-generator-app-main",
     },
     {
-      imgUrl: "/desktop-design3.jpg",
+      imgUrl: "desktop-design3.jpg",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisquerutrum sit amet metus id tristique. Fusce tristique nulla inelementum egestas. Aenean commodo turpis sapien, congue egestaslorem commodo sit amet. Quisque accumsan ante sed lacus accumsan dignissim.",
       liveUrl: "https://programistajarek.github.io/blogr-landing-page-main/",
       gitHubUrl: "https://github.com/ProgramistaJarek/blogr-landing-page-main",
     },
     {
-      imgUrl: "/desktop-design4.jpg",
+      imgUrl: "desktop-design4.jpg",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisquerutrum sit amet metus id tristique. Fusce tristique nulla inelementum egestas. Aenean commodo turpis sapien, congue egestaslorem commodo sit amet. Quisque accumsan ante sed lacus accumsan dignissim.",
       liveUrl:
